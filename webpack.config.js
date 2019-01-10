60579d0dcd4aa30fee5936a496dab1853320fa39
Rio <email4rio@gmail.com>
2019-01-10 09:49:14 +0700

    build with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,25 @@
 const path = require('path')
 
-module.exports = (env) => {
-  const config = {
-    entry: ['@babel/polyfill', path.join(__dirname, 'src', 'index.js')],
-    devtool: env.production ? 'source-map' : 'eval',
-    mode: env.production ? 'production' : 'development',
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: 'qiscus-sdk-core.min.js',
-      library: 'QiscusSDKCore',
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      umdNamedDefine: true
-    },
-    module: {
-      rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      }]
-    },
-    resolve: {
-      extensions: ['.js']
-    }
+module.exports = {
+  entry: ['@babel/polyfill', path.join(__dirname, 'src', 'index.js')],
+  devtool: 'source-map',
+  mode: 'production',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'qiscus-sdk-core.min.js',
+    library: 'QiscusSDKCore',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: /(node_modules|bower_components)/
+    }]
+  },
+  resolve: {
+    extensions: ['.js']
   }
-  return config
 }
