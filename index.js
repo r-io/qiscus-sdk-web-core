e36b263c7ad997530cb56e4726eee3de0b90ae6a
Rio <email4rio@gmail.com>
2019-01-23 14:47:30 +0700

    remove additional payload and type as qiscus does not support replying with image

--- index.js
+++ index.js
@@ -1135,7 +1135,7 @@ class QiscusSDK extends EventEmitter {
    * @returns Promise
    * @memberof QiscusSDK
    */
-  uploadFile(roomId, file, uniqueId, type, additionalPayload, onError) {
+  uploadFile(roomId, file, uniqueId, onError) {
     const self = this;
     var formData = new FormData();
     formData.append("file", file);
@@ -1155,8 +1155,8 @@ class QiscusSDK extends EventEmitter {
           roomId,
           `[file] ${url} [/file]`,
           uniqueId,
-          type,
-          JSON.stringify({ "url": url, ...additionalPayload }),
+          'file_attachment',
+          JSON.stringify({ "url": url }),
           null
         ).catch(error => onError(error));
       } else {
