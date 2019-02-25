ef54594e5ded76b63b665f93d00526c5007cd824
Rio <email4rio@gmail.com>
2019-02-25 12:20:10 +0700

    update cors

--- index.js
+++ index.js
@@ -1145,8 +1145,6 @@ class QiscusSDK extends EventEmitter {
     xhr.setRequestHeader("qiscus_sdk_app_id", `${self.AppId}`);
     xhr.setRequestHeader("qiscus_sdk_user_id", `${self.user_id}`);
     xhr.setRequestHeader("qiscus_sdk_token", `${self.userData.token}`);
-    xhr.setRequestHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
     xhr.onload = function () {
       if (xhr.status === 200) {
         // file(s) uploaded), let's post to comment
